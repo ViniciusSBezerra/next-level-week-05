548dd1b8f9dc1f48d5180be5783bf5c572748d23
vinicius <viniciussouza2599@gmail.com>
2021-04-20 11:10:08 -0300

    segundo dia da semana next level week 05

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,13 @@
 import express from "express"
 
+import './database'
+import { routes } from "./routes"
+
 const app = express();
 
 app.use(express.json())
+app.use(routes);
 
-app.get("/", (request, response) =>{
-    return response.json({
-        message: "primeira rota!"
-    })
-})
-
-app.post("/", (resquest, response) =>{
-    
-    response.json({
-        message: "usuario"
-    })
-
-    console.log("Usuarios",resquest.body)
-})
 
 app.listen(3333, () => {
     console.log(`Servidor no endereço http://localhost:3333`);
